dcbf5b1cd21186e26730fb7941b04479d52a8912
Diego Castaño <dowrow@outlook.com>
2021-09-17 18:46:36 +0200

    fix(index): update poem when style changes

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import styles from '../styles/Home.module.css'
 
@@ -52,7 +52,7 @@ export default function Home() {
     setPoem(poem);
   }
 
-  useState(() => {
+  useEffect(() => {
     generatePoem();
   }, [selectedStyle]);
 
